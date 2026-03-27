c489ef4c8b29cedf01dc8808cc703856748bd389
Ralchuk Vasyl <vasyl.ralchuk@gmail.com>
2026-03-27 13:07:13 +0200

    create Note repozitory

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import Mismatch from "@/lab/KeyMismatch.tsx";
+import Note from "@/entities/note/note";
+import Dz_3 from "@/lab/DZ_3";
+
+function App() {
+  return (
+    <>
+      <Dz_3 />
+      <Note />
+    </>
+  );
+}
+
+export default App;
