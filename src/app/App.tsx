a513a36414453bc582bd096f384922913a682882
Vasyl Ralchuk <vasyl.ralchuk@gmail.com>
2026-04-01 19:14:03 +0300

    create targetCapture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 // import { useState } from "react";
 import Mismatch from "@/lab/KeyMismatch.tsx";
 import Note from "@/entities/note/note";
-import Dz_3 from "@/lab/DZ_3";
+// import Dz_3 from "@/lab/DZ_3";
+import TergetCapture from "@/lab/TargetCapture";
 
 function App() {
   return (
     <>
-      <Dz_3 />
-      <Note />
+      <TergetCapture/>
     </>
   );
 }
